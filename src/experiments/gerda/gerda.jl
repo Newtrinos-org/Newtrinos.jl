93ce0f786b49a68a31389882c658c39c0b92f02d
David Schultheiß <d.schultheiss@tum.de>
2026-09-14 23:43:37 +0200

    forgot to remove "_SM" from function name

--- src/experiments/gerda/gerda.jl
+++ src/experiments/gerda/gerda.jl
@@ -56,7 +56,7 @@ function get_assets(physics; datadir = @__DIR__)
 end
 
 
-function get_neutrinomass_SM(cfg::Newtrinos.osc.ThreeFlavour)
+function get_neutrinomass(cfg::Newtrinos.osc.ThreeFlavour)
     function NeutrinoMass_SM(params::NamedTuple)
 
         U=  Newtrinos.osc.get_PMNS(params)
