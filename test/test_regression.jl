91f746c8a6f86078fe990c84d59a0830328a9711
David Schultheiß <d.schultheiss@tum.de>
2026-09-15 01:21:47 +0200

    Update katrin and gerda values in test_regression

--- test/test_regression.jl
+++ test/test_regression.jl
@@ -14,8 +14,8 @@ const ALL_REFERENCE_VALUES = (
     orca         = -1164.2506083927215,
     coherent_csi = -574.3416032522271,
     coherent_lAr = -1754.9946939034153,
-    katrin       = 0.45490017795768,
-    gerda        = -85.62009498969059,
+    katrin       = 0.4549394122046937,
+    gerda        = -85.50961704258683,
 )
 
 # Parse --experiments from ARGS if present, otherwise run all
